5e098ff43a4ebae7d9f30779f90de53a24f2a852
Edmétrio J <edmetrio06@gmail.com>
2021-12-27 13:59:36 +0000

    Update Transacao

--- app/Http/Controllers/HistoricoController.php
+++ app/Http/Controllers/HistoricoController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Models\Historico;
 use App\Models\Models\Itemhistorico;
 use App\Models\Models\Itemtransacao;
-use App\Models\Models\transacao;
+use App\Models\Models\Transacao;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -17,7 +17,7 @@ class HistoricoController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-   
+
 
     public function index()
     {
@@ -43,27 +43,66 @@ class HistoricoController extends Controller
     public function store(Request $request)
     {
         /* return $request->all(); */
-        $user = Auth::user()->id;
-        $transacao = transacao::latest()->where('users_id', $user)->first();
-        $item = Itemtransacao::where('transacao_id', $request->transacao_id)->get();
-        $historico = Historico::create([
-            'users_id' => $user,
-            'pagamento_id' => $request->pagamento_id,
-            'valortotal' => $request->valortotal
-        ]);
-        foreach($item as $itens)
-        {
-            Itemhistorico::create([
-                'historico_id' => $historico->id,
-                'artigo_id' => $itens->artigo_id,
-                'quantidade' => $itens->quantidade,
+
+        if (isset($request->gerar)) {
+
+            $user = Auth::user()->id;
+            $transacao = Transacao::latest()->where('users_id', $user)->first();
+            $item = Itemtransacao::where('transacao_id', $request->transacao_id)->get();
+            $historico = Historico::create([
+                'users_id' => $user,
+                'pagamento_id' => $request->pagamento_id,
+                'valortotal' => $request->valortotal
             ]);
+            foreach ($item as $itens) {
+                Itemhistorico::create([
+                    'historico_id' => $historico->id,
+                    'artigo_id' => $itens->artigo_id,
+                    'quantidade' => $itens->quantidade,
+                ]);
+            }
+            $$transacao = Transacao::latest()->where('users_id', $user)->delete();
+            $item = Itemtransacao::where('transacao_id', $request->transacao_id)->delete();
+
+            $hist = Historico::latest()->with(['users', 'pagamentos'])
+                ->where('users_id', Auth::user()->id)->first();
+            $historico = Itemhistorico::orderBy('id', 'desc')
+                ->where('historico_id', $hist->id)
+                ->with(['historicos', 'artigos'])
+                ->get();
+
+            $total = 0;
+            foreach ($historico as $h) {
+                $h->subtotal = $h->artigos->preco * $h->quantidade;
+                $total += $h->subtotal;
+            }
+            $historico->total = $total;
+            $iva = $total * 0.17;
+            $historico->tt = $iva + $total;
+
+
+            return view('invoide', compact('historico', 'hist'));
+        } else {
+            $user = Auth::user()->id;
+            $transacao = Transacao::latest()->where('users_id', $user)->first();
+            $item = Itemtransacao::where('transacao_id', $request->transacao_id)->get();
+            $historico = Historico::create([
+                'users_id' => $user,
+                'pagamento_id' => $request->pagamento_id,
+                'valortotal' => $request->valortotal
+            ]);
+            foreach ($item as $itens) {
+                Itemhistorico::create([
+                    'historico_id' => $historico->id,
+                    'artigo_id' => $itens->artigo_id,
+                    'quantidade' => $itens->quantidade,
+                ]);
+            }
+            $$transacao = Transacao::latest()->where('users_id', $user)->delete();
+            $item = Itemtransacao::where('transacao_id', $request->transacao_id)->delete();
+            $request->session()->flash('status', 'Venda Finalizada!');
+            return redirect('vendas');
         }
-        $$transacao = transacao::latest()->where('users_id', $user)->delete();
-        $item = Itemtransacao::where('transacao_id', $request->transacao_id)->delete();
-        $request->session()->flash('status', 'Venda Finalizada!');
-        return redirect('vendas');
-        
     }
 
     /**
