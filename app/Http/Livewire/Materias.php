82949104916c9284674ec6cd29006159b0c51f57
Edmétrio J <edmetrio06@gmail.com>
2022-01-01 15:36:08 +0000

    livewire materias

---
+++ app/Http/Livewire/Materias.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Models\Armazem;
+use App\Models\Models\Unidade;
+use App\Models\Models\Categoria;
+use App\Models\Models\Materia;
+use App\Models\Models\Stock;
+use Livewire\Component;
+
+class Materias extends Component
+{
+    public function mount()
+    {
+        $this->categoria = Categoria::orderBy('id', 'desc')->get();
+    }
+
+    public function render()
+    {
+        $unidade = Unidade::orderBy('id', 'desc')->get();
+        $armazem = Armazem::orderBy('id', 'desc')->get();
+
+        $materia = Materia::orderBy('id', 'desc')->with(['stocks'])->get();
+        return view('livewire.materias', compact('unidade','armazem','materia'));
+
+    }
+
+    public function armazems()
+    {
+        return $this->hasManyThrough(Armazem::class, Stock::class, 'materia_id', 'armazem_id', 'id', 'id');
+    }
+}
