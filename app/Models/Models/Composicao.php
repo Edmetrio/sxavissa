4967d0bcdb090ea809344ca627f293501679ed07
Edmétrio J <edmetrio06@gmail.com>
2022-01-01 15:36:47 +0000

    relarion artigo

--- app/Models/Models/Composicao.php
+++ app/Models/Models/Composicao.php
@@ -15,11 +15,16 @@ class Composicao extends Model
     protected $guarded = [];
 
     protected $table = 'composicao';
-    protected $fillable = ['artigo_id','unidade_id','quantidade','estado'];
+    protected $fillable = ['artigo_id','materia_id','unidade_id','quantidade','estado'];
 
     public function artigos()
     {
         return $this->hasOne(Artigo::class, 'id', 'artigo_id');
+    }
+
+    public function materias()
+    {
+        return $this->hasOne(Materia::class, 'id', 'materia_id');
     }
 
     public function unidades()
