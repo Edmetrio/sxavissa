ea1fa0a5a9a2c5dff70eebd66819ebd8ae6f0338
Edmétrio J <edmetrio06@gmail.com>
2022-01-01 15:39:23 +0000

    add transacao_id

--- database/migrations/2021_09_08_115454_create_aumentos_table.php
+++ database/migrations/2021_09_08_115454_create_aumentos_table.php
@@ -15,12 +15,18 @@ class CreateAumentosTable extends Migration
     {
         Schema::create('aumento', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->uuid('users_id')->nullable();
+            $table->foreign('users_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->uuid('artigo_id')->nullable();
             $table->foreign('artigo_id')->references('id')->on('artigo')->onDelete('cascade')->onUpdate('cascade');
+            $table->uuid('pagamento_id')->nullable();
+            $table->foreign('pagamento_id')->references('id')->on('pagamento')->onDelete('cascade')->onUpdate('cascade');
             $table->uuid('unidade_id')->nullable();
             $table->foreign('unidade_id')->references('id')->on('unidade')->onDelete('cascade')->onUpdate('cascade');
-            $table->uuid('transacao_id')->nullable();
-            $table->foreign('transacao_id')->references('id')->on('transacao')->onDelete('cascade')->onUpdate('cascade');
+            $table->uuid('materia_id')->nullable();
+            $table->foreign('materia_id')->references('id')->on('materia')->onDelete('cascade')->onUpdate('cascade');
+            $table->uuid('armazem_id')->nullable();
+            $table->foreign('armazem_id')->references('id')->on('armazem')->onDelete('cascade')->onUpdate('cascade');
             $table->string('numerolote')->nullable();
             $table->decimal('custo', 20,2)->nullable();
             $table->decimal('quantidade', 20,2)->nullable();
