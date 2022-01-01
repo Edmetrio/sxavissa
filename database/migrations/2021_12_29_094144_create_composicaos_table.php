ec98c7c9baab40d1976f0662d8f83881cc4013ee
Edmétrio J <edmetrio06@gmail.com>
2022-01-01 15:41:09 +0000

    add table composicao

---
+++ database/migrations/2021_12_29_094144_create_composicaos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComposicaosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('composicao', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('artigo_id')->nullable();
+            $table->foreign('artigo_id')->references('id')->on('artigo')->onDelete('cascade')->onUpdate('cascade');
+            $table->uuid('materia_id')->nullable();
+            $table->foreign('materia_id')->references('id')->on('materia')->onDelete('cascade')->onUpdate('cascade');
+            $table->uuid('unidade_id')->nullable();
+            $table->foreign('unidade_id')->references('id')->on('unidade')->onDelete('cascade')->onUpdate('cascade');
+            $table->decimal('quantidade', 20,2)->nullable();
+            $table->string('estado')->default('on')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('composicao');
+    }
+}
