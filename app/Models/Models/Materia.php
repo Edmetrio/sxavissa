b4b7c25dde04e78977d0552300eefc9469590645
Edmétrio J <edmetrio06@gmail.com>
2022-01-01 15:37:20 +0000

    model materia

---
+++ app/Models/Models/Materia.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+
+class Materia extends Model
+{
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'materia';
+    protected $fillable = ['subcategoria_id','nome','preco','estado'];
+
+    public function stocks()
+    {
+        return $this->hasMany(Stock::class, 'materia_id');
+    }
+
+    public function armazems()
+    {
+        return $this->hasOneThrough(Armazem::class, Stock::class);
+    }
+}
