a409a321af05d214263ccfff771ce405a2b92f29
Edmétrio J <edmetrio06@gmail.com>
2022-01-07 15:09:31 +0000

    livewire composicao

--- app/Http/Livewire/Composicaos.php
+++ app/Http/Livewire/Composicaos.php
@@ -10,15 +10,33 @@ use Livewire\Component;
 
 class Composicaos extends Component
 {
-
-    public $search = '';
+    public $artigo;
+    public $composicao;
+    
+    public $selectedArtigo = NULL;
+    
+    public function mount()
+    {
+        $this->artigo = Artigo::orderBy('created_at', 'desc')
+                                ->where('tipo_id', '3ce23584-56cc-45ce-853d-84c9965053bf')
+                                ->get();
+        $this->composicao = collect();
+    }
 
     public function render()
     {
-        $composicao = Composicao::with(['artigos','materias','unidades'])->where('quantidade', '=' ,'%'.$this->search.'%')->get();
-        $composicao->artigo = Artigo::orderBy('id', 'desc')->get();
-        $composicao->unidade = Unidade::orderBy('id', 'desc')->get();
-        $composicao->materia = Materia::orderBy('id', 'desc')->get();
-        return view('livewire.composicaos', compact('composicao'));
+        $composic = Composicao::with(['artigos','materias','unidades'])
+                        ->orderBy('created_at', 'desc')->get();
+        $composic->artigo = Artigo::orderBy('created_at', 'desc')->where('tipo_id', '3ce23584-56cc-45ce-853d-84c9965053bf')->get();
+        $unidade = Unidade::orderBy('created_at', 'desc')->get();
+        $materia = Materia::orderBy('created_at', 'desc')->get();
+        return view('livewire.composicaos', compact('composic','materia','unidade'));
+    }
+
+    public function updatedSelectedArtigo($artigo_id)
+    {
+        if(!is_null($artigo_id)){
+            $this->composicao = Composicao::where('artigo_id', $artigo_id)->get();
+        }
     }
 }
